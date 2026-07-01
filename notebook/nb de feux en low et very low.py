52e287fa40d1cb5b7f791cb6fbeb9493479a6f67
Amanda Verde Mota <amanda.verde_mota@etu.minesparis.psl.eu>
2026-07-01 10:15:21 +0200

    je recommit

--- notebook/nb de feux en low et very low.py
+++ notebook/nb de feux en low et very low.py
@@ -4,6 +4,10 @@ df = pd.read_csv("feux_et_fwi.csv")
 
 # Filtrer les incendies avec fwi_class "low" ou "very_low"
 df_filtre = df[df["fwi_class"].isin(["low", "very_low"])].copy()
+df_extreme = df[df["fwi_class"] == "extreme"].copy()
+
+#Nombre incendies ene extreme 
+print(f"Nombre d'incendies (extreme) : {len(df_extreme)}")
 
 # Nombre d'incendies restants
 print(f"Nombre d'incendies (low + very_low) : {len(df_filtre)}")
